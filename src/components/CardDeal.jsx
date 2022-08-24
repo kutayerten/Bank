cc5d47474eac3a41996ce1530dc597a07009d95e
kutayerten <kutayerten@gmail.com>
2022-08-24 12:57:25 +0300

    Bank Website With React & Tailwind

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,24 @@
+import { card } from "../assets";
+import styles, {layout} from "../style";
+import Button from "./Button";
+
+const CardDeal = () =>(
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2>Find a better card deal <br className="sm:block hidden" /> in few easy steps with kTy Bank</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Contrary to popular belief, Lorem Ipsum is not simply random text. 
+          It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+          </p>
+          <Button styles="mt-10" />
+      </div>
+    
+
+    <div className={layout.sectionImg}>
+
+    <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    
+    </div>
+    </section>
+  )
+
+export default CardDeal
